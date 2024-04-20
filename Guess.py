3446b57616f56a1061b877b4ddac8bb7562ea2fe
ShinsouBlu <93885352+ShinsouBlu@users.noreply.github.com>
2024-04-19 21:18:26 -0500

    Initial Commit

---
+++ Guess.py
@@ -0,0 +1,22 @@
+#import statements
+import random
+
+answer=random.randint(1,100)
+numGuesses=0
+guess=0
+while guess != answer:
+    numGuesses+=1
+    try:
+        guess=int(input("Please input your guess (1-100):"))
+    except ValueError:
+        print("Invalid input. You must input a number between 1 and 100")
+        continue
+    if guess < 1 or guess > 100:
+        print("Invalid input. You must input a number between 1 and 100")
+    elif guess > answer:
+        print("Incorrect. Answer is lower!")
+    elif guess < answer:
+        print("Incorrect. Answer is higher!")
+    else:
+        print("You got it!")
+        print("Number of guess taken: " + str(numGuesses))
